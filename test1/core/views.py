f2f8d71d8040c62552933b1da9bba07a630a2d55
wheelere <wheelere@grinnell.edu>
2016-08-03 11:05:40 -0400

    Figured out xml formatting. Trying functionality.

--- test1/core/views.py
+++ test1/core/views.py
@@ -140,24 +140,22 @@ def handle_inbound(request):
 		dict = xmltodict.parse(request.POST.get("XMLDATA"))
 		message = dict["IncomingRequest"]["IncomingMessage"]["Message"]
 		number = dict["IncomingRequest"]["IncomingMessage"]["Phonenumber"]
-		logger.info("Number is %s" % number)
-		logger.info("Message is '%s'" % message)
-		# message = request.POST.get("IncomingMessage")
-		# number = request.POST.get("Phonenumber")
-		# if "SJBKXG" in message:
-		# 	u = User.objects.filter(number=number)
-		# 	try:
-		# 		u = u.get()
-		# 	except DoesNotExist:
-		# 		logger.warning("Received inbound verify from an unlisted number.")
-		# 		return HttpResponseRedirect('/')
-		# 	except MultipleObjectsReturned:
-		# 		logger.error("The number " + number + " has multiple User listings.")
-		# 		return HttpResponseRedirect('/')
-		# 	u.verified = True
-		# 	u.save()
-		# 	e = Event(type="verify_user",
-		# 		time_occurred=datetime.now(),
-		# 		user_id=u.id)
-		# 	e.save()
+		logger.info("Received Message from number '%s'. \Message is: '%s'"
+					% (number, message)
+		if "SJBKXG" in message:
+			u = User.objects.filter(number=number)
+			try:
+				u = u.get()
+			except DoesNotExist:
+				logger.warning("Received inbound verify from an unlisted number.")
+				return HttpResponseRedirect('/')
+			except MultipleObjectsReturned:
+				logger.error("The number " + number + " has multiple User listings.")
+				return HttpResponseRedirect('/')
+			u.verified = True
+			u.save()
+			e = Event(type="verify_user",
+				time_occurred=datetime.now(),
+				user_id=u.id)
+			e.save()
 	return HttpResponseRedirect('/')
