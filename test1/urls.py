daaf2516b0db332138a412eb53d86c8722800926
wheelere <wheelere@grinnell.edu>
2016-08-23 15:54:19 -0400

    working on issues with static files

--- test1/urls.py
+++ test1/urls.py
@@ -35,6 +35,4 @@ urlpatterns = [
     url(r'^outbox/$', views.outbound_message),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
