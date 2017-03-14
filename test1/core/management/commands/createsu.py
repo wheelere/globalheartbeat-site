4337fd936d1da3a74177395ea093a6ac2b85c2b3
wheelere <wheelere@grinnell.edu>
2017-03-14 13:32:51 -0400

    hid confidential information from settings; added the gitignore

--- test1/core/management/commands/createsu.py
+++ test1/core/management/commands/createsu.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # This command sets up an admin user if none exists
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "globalheartbeat@umich.edu", "G1oba1Heartbeat")
+            User.objects.create_superuser("admin", settings.SUPERUSER_ACCOUNT, settings.SUPERUSER_PASS)
